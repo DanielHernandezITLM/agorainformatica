8fa66b7a5228e7cb55f43746d8b3e789f79bf263
DanielHernandezITLM <danihm95@gmail.com>
2017-03-29 23:46:31 -0700

    Página nueva y actualizaciones
    Se agrega la página de investigacion, con unas nuevas imágenes
    se cambia un poco el código de index

---
+++ SitioWeb-AgoraInformatica/JavaScript/investigacion/investigacion.js
@@ -0,0 +1,24 @@
+function show() {    
+document.getElementById('areainvestigacionIMG').style.visibility='visible';    
+}
+function hide(){
+document.getElementById('areainvestigacionIMG').style.visibility="hidden"
+}
+function show2() {    
+document.getElementById('lineainvestigacionIMG').style.visibility='visible';    
+}
+function hide2(){
+document.getElementById('lineainvestigacionIMG').style.visibility="hidden"
+}
+function show3() {    
+document.getElementById('cargoinvestigacionIMG').style.visibility='visible';    
+}
+function hide3(){
+document.getElementById('cargoinvestigacionIMG').style.visibility="hidden"
+}
+function show4() {    
+document.getElementById('titulosinvestigacionIMG').style.visibility='visible';    
+}
+function hide4(){
+document.getElementById('titulosinvestigacionIMG').style.visibility="hidden"
+}
